7a531e380d74f2e960f4e226a54c8756af5c650c
Mattie Schraeder <mattie@mattie.lgbt>
2023-04-21 13:25:04 -0500

    make this null

--- theshadowbook-frontend/src/app/services/backend.service.ts
+++ theshadowbook-frontend/src/app/services/backend.service.ts
@@ -21,8 +21,8 @@ export class BackendService {
         }
       });
     } else {
-      let result = new Subject<boolean>;
-      result.next(false);
+      let result = new Subject<any>;
+      result.next(null);
       result.complete();
 
       return result.asObservable();
