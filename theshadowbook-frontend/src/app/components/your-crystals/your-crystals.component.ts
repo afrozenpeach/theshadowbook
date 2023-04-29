bdbfcb7c0b08deb64441f04ab2df5e149fdaece8
Mattie Schraeder <mattie@mattie.lgbt>
2023-04-29 06:55:52 -0500

    working your-crystals with subtypes

--- theshadowbook-frontend/src/app/components/your-crystals/your-crystals.component.ts
+++ theshadowbook-frontend/src/app/components/your-crystals/your-crystals.component.ts
@@ -18,10 +18,13 @@ export class YourCrystalsComponent {
   shapes: any = [];
   subTypes: any = [];
 
-  crystalForm: FormGroup[] = [];
+  crystalForms: FormGroup[] = [];
   addCrystalForm: FormGroup = new FormGroup({
     crystal: new FormControl('')
   });
+
+  userCrystalsOfType: any = [];
+  subTypesOfCrystal: any = [];
 
   loading = true;
 
@@ -30,52 +33,76 @@ export class YourCrystalsComponent {
   }
 
   ngOnInit() {
-    this.backendService.getUser().subscribe(u => {
-      this.user = u.user;
+    this.backendService.getColors().subscribe(c => {
+      this.colors = c.colors;
 
-      this.backendService.getCrystals().subscribe(c => {
-        this.crystals = c.crystals;
+      this.backendService.getStatuses().subscribe(s => {
+        this.statuses = s.statuses;
 
-        this.backendService.getUserCrystals(this.user.id).subscribe(uc => {
-          this.userCrystals = uc.crystals;
+        this.backendService.getShapes().subscribe(s => {
+          this.shapes = s.shapes;
 
-          this.backendService.getColors().subscribe(c => {
-            this.colors = c.colors;
+          this.backendService.getUser().subscribe(u => {
+            this.user = u.user;
 
-            this.backendService.getStatuses().subscribe(s => {
-              this.statuses = s.statuses;
+            this.backendService.getUserCrystals(this.user.id).subscribe(uc => {
+              for (let userCrystal of uc.crystals) {
+                this.crystalForms[userCrystal.id] = new FormGroup({
+                  id: new FormControl(userCrystal.id),
+                  name: new FormControl(userCrystal.name),
+                  primaryColor: new FormControl(userCrystal.primaryColor),
+                  secondaryColor: new FormControl(userCrystal.secondaryColor),
+                  tertiaryColor: new FormControl(userCrystal.tertiaryColor),
+                  sizeX: new FormControl(userCrystal.sizeX),
+                  sizeY: new FormControl(userCrystal.sizeY),
+                  sizeZ: new FormControl(userCrystal.sizeZ),
+                  weight: new FormControl(userCrystal.weight),
+                  karat: new FormControl(userCrystal.karat),
+                  cut: new FormControl(userCrystal.cut),
+                  aura: new FormControl(userCrystal.aura),
+                  status: new FormControl(userCrystal.status),
+                  shape: new FormControl(userCrystal.shape),
+                  notes: new FormControl(userCrystal.notes),
+                  subType: new FormControl(userCrystal.subType)
+                });
 
-              this.backendService.getShapes().subscribe(s => {
-                this.shapes = s.shapes;
+                if (!this.userCrystalsOfType[userCrystal.crystal]) {
+                  this.userCrystalsOfType[userCrystal.crystal] = [];
+                }
 
-                this.backendService.getCrystalSubTypes().subscribe(s => {
-                  this.subTypes = s.crystalSubTypes
+                if (!this.userCrystalsOfType[userCrystal.crystal][userCrystal.subType ?? 'none']) {
+                  this.userCrystalsOfType[userCrystal.crystal][userCrystal.subType ?? 'none'] = [];
+                }
+
+                this.userCrystalsOfType[userCrystal.crystal][userCrystal.subType ?? 'none'].push(userCrystal);
+              }
+
+              this.userCrystals = uc.crystals;
+
+              this.userCrystals.map((c: { id: string | number; }) => {
+                this.subTypesOfCrystal[c.id] = [];
+                this.subTypesOfCrystal[c.id].push({
+                  id: -1,
+                  crystal: c.id,
+                  type: ''
+                });
+              });
+
+              this.backendService.getCrystalSubTypes().subscribe(s => {
+                this.subTypes = s.crystalSubTypes
+
+                this.subTypes.map((st: {id: any; crystal: string | number; }) => {
+                  this.subTypesOfCrystal[st.crystal].push(st);
+                });
+
+                this.backendService.getCrystals().subscribe(c => {
+                  this.crystals = c.crystals;
+
                   this.loading = false;
                 });
               });
             });
           });
-
-          for (let userCrystal of this.userCrystals) {
-            this.crystalForm[userCrystal.id] = new FormGroup({
-              id: new FormControl(userCrystal.id),
-              name: new FormControl(userCrystal.name),
-              primaryColor: new FormControl(userCrystal.primaryColor),
-              secondaryColor: new FormControl(userCrystal.secondaryColor),
-              tertiaryColor: new FormControl(userCrystal.tertiaryColor),
-              sizeX: new FormControl(userCrystal.sizeX),
-              sizeY: new FormControl(userCrystal.sizeY),
-              sizeZ: new FormControl(userCrystal.sizeZ),
-              weight: new FormControl(userCrystal.weight),
-              karat: new FormControl(userCrystal.karat),
-              cut: new FormControl(userCrystal.cut),
-              aura: new FormControl(userCrystal.aura),
-              status: new FormControl(userCrystal.status),
-              shape: new FormControl(userCrystal.shape),
-              notes: new FormControl(userCrystal.notes),
-              subType: new FormControl(userCrystal.subType)
-            });
-          }
         });
       });
     });
@@ -85,12 +112,18 @@ export class YourCrystalsComponent {
     return this.userCrystals.filter((uc: {crystal: number;}) => uc.crystal === id);
   }
 
+  getSubTypesOfCrystal(id: number) {
+    let returnValue = this.subTypes.filter((s: { crystal: number; }) => s.crystal === id);
+    returnValue.unshift({id: null, crystal: id, type: ''});
+    return returnValue;
+  }
+
   getSubTypesOfType(id: number) {
     return this.subTypes.filter((s: { crystal: number; }) => s.crystal === id);
   }
 
   saveUserCrystal(id: number) {
-    this.backendService.saveUserCrystal(this.crystalForm[id].value).subscribe(s => {
+    this.backendService.saveUserCrystal(this.crystalForms[id].value).subscribe(s => {
 
     });
   }
@@ -104,13 +137,13 @@ export class YourCrystalsComponent {
   }
 
   saveDisabled(id: number) {
-    return !this.crystalForm[id].valid;
+    return !this.crystalForms[id].valid;
   }
 
   addUserCrystal(status: number) {
     if (this.addCrystalForm.controls['crystal'].value) {
       this.backendService.addCrystalToCollection(this.addCrystalForm.controls['crystal'].value, this.user.id, status).subscribe(s => {
-        this.crystalForm[s.crystal.id] = new FormGroup({
+        this.crystalForms[s.crystal.id] = new FormGroup({
           id: new FormControl(s.crystal.id),
           name: new FormControl(''),
           primaryColor: new FormControl(null),
@@ -139,7 +172,7 @@ export class YourCrystalsComponent {
     let subscriptions: Observable<any>[] = [];
 
     this.userCrystals.map((uc: { id: number; }) => {
-      subscriptions.push(this.backendService.saveUserCrystal(this.crystalForm[uc.id].value));
+      subscriptions.push(this.backendService.saveUserCrystal(this.crystalForms[uc.id].value));
     });
 
     zip(...subscriptions).subscribe(() => {
