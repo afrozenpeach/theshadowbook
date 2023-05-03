aa969b3e654c0bf5e5bde4b7d88ba0e95d0fe6f6
Mattie Schraeder <mattie@mattie.lgbt>
2023-05-03 15:02:10 -0500

    colors should be unique

---
+++ theshadowbook-backend/migrations/20230503200020-fixColorNameUniqueness.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addIndex('Color', ['color'], {
+      name: 'Color-UNIQUE',
+      unique: true
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
